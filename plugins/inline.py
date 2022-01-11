d699c286078fadd6bd364157c0b1ed8605282ffd
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2022-01-11 14:23:33 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('Mean Channel', url='https://t.me/Hindi_world_series')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
