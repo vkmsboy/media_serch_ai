5baeae9f51f688c90ab1599005f434ba78a7ec7e
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2022-01-21 12:56:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
+import os
 import logging
 import logging.config
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+from pyromod import listen
+from pyrogram import Client
+from config import*
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from utils import Media
@@ -35,6 +40,18 @@ class Bot(Client):
         await super().stop()
         print("Bot stopped. Bye.")
 
+    def main():
+    plugins = dict(root="plugins")
+    app = Client("FileStore",
+                 bot_token=BOT_TOKEN,
+                 api_id=API_ID,
+                 api_hash=API_HASH,
+                 plugins=plugins,
+                 workers=100)
+
 
 app = Bot()
 app.run()
+
+if name == "main":
+    main()
